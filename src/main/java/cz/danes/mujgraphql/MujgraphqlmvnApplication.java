dbde167af39aaa10a77f60b5b34c846923549e6b
vachad <daniel.vacha@kctdata.cz>
2020-10-27 20:26:04 +0100

    default kommit stejne .java ale mvn jee depenednci

---
+++ src/main/java/cz/danes/mujgraphql/MujgraphqlmvnApplication.java
@@ -0,0 +1,13 @@
+package cz.danes.mujgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujgraphqlmvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MujgraphqlmvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/danes/mujgraphql/MujgraphqlmvnApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.danes.mujgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujgraphqlmvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
