c29613429896bfcfaa869ad9f510647e6b58e9c2
vachad <daniel.vacha@kctdata.cz>
2020-10-30 22:15:37 +0100

    nova trida personinfo pro oe2one plneni db obou tabulek ok, funguje  personinfo pridana trida PersonInfoQuery bez toho to nejde

---
+++ src/main/java/cz/danes/mujgraphql/resolver/PersonInfoQuery.java
@@ -0,0 +1,20 @@
+package cz.danes.mujgraphql.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import cz.danes.mujgraphql.model.Person;
+import cz.danes.mujgraphql.model.PersonInfo;
+import cz.danes.mujgraphql.repositories.PersonInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PersonInfoQuery implements GraphQLQueryResolver {
+    private final PersonInfoRepository repo;
+
+
+    public Iterable<PersonInfo> allPersonInfo() {
+        return repo.findAll();
+    }
+
+}
--- src/main/java/cz/danes/mujgraphql/resolver/PersonQuery.java
+++ src/main/java/cz/danes/mujgraphql/resolver/PersonQuery.java
@@ -3,18 +3,26 @@ package cz.danes.mujgraphql.resolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import cz.danes.mujgraphql.exception.PersonNotFoundException;
 import cz.danes.mujgraphql.model.Person;
+import cz.danes.mujgraphql.model.PersonInfo;
+import cz.danes.mujgraphql.repositories.PersonInfoRepository;
 import cz.danes.mujgraphql.repositories.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class PersonQuery implements GraphQLQueryResolver {
 
     private final PersonRepository repo;
+    private final PersonInfoRepository repopi;
 
     public Person person(Long id) {
-        return repo.findById(id).orElseThrow(() -> new PersonNotFoundException("person not found", id));
+        Optional<PersonInfo> repopiById = repopi.findById(id);
+        Person person = repo.findById(id).orElseThrow(() -> new PersonNotFoundException("person not found", id));
+        person.setPersonInfo(repopiById.get());
+        return person;
     }
 
     public Iterable<Person> allPeople() {
